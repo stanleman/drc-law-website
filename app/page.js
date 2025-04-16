42f17a42d9cfd23d15a02234ddcab75f5293a5c7
stanleman <stanlord2009@gmail.com>
2025-04-16 19:42:46 +0800

    updated My People page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,12 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 export default function Home() {
   return (
     <main className="bg-white">
@@ -30,9 +39,63 @@ export default function Home() {
               an optimistic voice.
             </p>
 
-            <button className="bg-[#BF8033] px-5 py-3 mt-5">
-              Book a consultation
-            </button>
+            <Dialog>
+              <DialogTrigger>
+                <div className="bg-[#BF8033] px-5 py-3 mt-5">
+                  Book a consultation
+                </div>
+              </DialogTrigger>
+              <DialogContent className="max-w-lg p-6 bg-white rounded-lg shadow-lg">
+                <DialogHeader>
+                  <DialogTitle className="text-3xl  text-gray-900">
+                    Contact us
+                  </DialogTitle>
+                </DialogHeader>
+                <form className="">
+                  <label className="font-medium text-gray-700">Name</label>
+                  <input
+                    type="text"
+                    placeholder="Your name"
+                    className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+
+                  <label className="font-medium text-gray-700 ">Email</label>
+                  <input
+                    type="email"
+                    placeholder="Your email"
+                    className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+
+                  <label className="font-medium text-gray-700">
+                    Phone number
+                  </label>
+                  <input
+                    type="tel"
+                    placeholder="Your phone number"
+                    className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+
+                  <div>
+                    <label className="block mb-1 font-medium text-gray-700">
+                      Message
+                    </label>
+                    <textarea
+                      id="message"
+                      placeholder="Your message"
+                      rows="4"
+                      className="w-full px-4 py-2 mb-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    ></textarea>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="w-full bg-[#152F52] text-white py-2 rounded-md font-semibold hover:bg-[#1c3b6f] transition"
+                  >
+                    Submit
+                  </button>
+                </form>
+              </DialogContent>
+            </Dialog>
           </div>
         </div>
       </div>
--- app/people/page.js
+++ app/people/page.js
@@ -6,32 +6,30 @@ const employees = [
   {
     name: "Bryan Lloyd L. Dela Rosa",
     position: "Managing Partner",
-    image: "/bryan.jpg",
+    image: "/Bryan4.jpg",
   },
   {
     name: "Paul Vincent M. Casilla",
     position: "Partner",
-    image: "/paul.jpg",
+    image: "/Paul4.jpg",
   },
   {
     name: "Beatriz Zamora",
     position: "Associate",
-    image: "/beatriz.jpg",
+    image: "/BMZ.jpg",
+    bio: "With more than a year’s worth of experience in the legal field, Beatriz’s (Bei) practice areas include litigation, corporate law, taxation, labor & employment, and mergers & acquisitions. After graduating from the UP College of Law in 2023 and subsequently passing the Bar Examinations in the same year, Bei started her legal career as a junior associate in Sarmiento Loriega Law Office, a medium-sized law firm in Ortigas specializing in corporate and taxation law. In her first year as a lawyer, Bei was exposed to the various aspects of corporate, taxation, and litigation practice with the guidance of lawyers consistently recognized by the Asian Legal Business Awards and the Top 100 Lawyers in the Philippines (ABLJ). She joined DC Law in January 2025 and currently rotates in the firm’s various practice areas. Bei believes in the constant pursuit of learning, considering she spends most of her spare time reading and less of it writing. True to her roots, she is always in search of the next book to add to her growing collection.",
   },
   {
     name: "Bienvenido Co",
     position: "Associate",
-    image: "/bienvenido.jpg",
-  },
-  {
-    name: "Paolo Villanueva",
-    position: "Associate",
-    image: "/paolo.jpeg",
+    image: "/BMC.jpg",
+    bio: "Bienvenido’s (Ben) practice areas include litigation, corporate law, mergers & acquisitions, real estate, and labor & employment. Ben started his legal career at Gatmaytan Yap Patacsil Gutierrez & Protacio (C&G Law) where he was an underbar associate from January 2023 to May 2023 and then associate from May 2023 to July 2024. He joined DC Law on September 2024 and is heavily involved in the firm’s litigation practice area.",
   },
   {
     name: "Restine Joy Armada",
     position: "Associate",
     image: "/restine.jpg",
+    bio: "Restine is specializing in litigation, corporate law, administrative cases, real estate, estate settlement, election law, intellectual property and labor and employment law. With a vigorous track record of representing clients in various legal matters, Atty. Armada has built a reputation for delivering strategic and effective legal solutions. Her extensive experience spans both corporate and individual sectors, offering expert counselling in navigating complex legal landscapes, resolving disputes, and ensuring compliance with regulatory frameworks. Committed to providing diligent and personalized service, Atty. Armada continues to uphold the highest standards of legal excellence in her practice. Restine began her legal career at Acube Legal Support, Inc. and Jose Antonio Aliling & Associates (ACUBE Law), where she worked as an underbar associate from February 2023 to May 2023, followed by an associate role from May 2023 to August 2023. She joined DC Law in September 2023 and plays an active role in the firm’s litigation practice.",
   },
 ];
 
@@ -45,12 +43,11 @@ function People() {
 
   return (
     <main className="p-5 lg:p-10">
-      <h1 className="font-serif text-3xl md:text-4xl lg:text-5xl mb-5 md:mb-7 lg:mb-10">
+      <h1 className="font-serif text-3xl md:text-4xl lg:text-5xl mb-8">
         Our People
       </h1>
 
-      {/* Tabs */}
-      <div className="flex space-x-6 mb-8 text-lg font-semibold">
+      <div className="flex justify-center mb-10 text-xl font-semibold space-x-24">
         <button
           onClick={() => setSelectedTab("Partners")}
           className={`border-b-2 ${
@@ -73,25 +70,57 @@ function People() {
         </button>
       </div>
 
-      {/* Cards */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {filteredEmployees.map((person, index) => (
-          <div
-            key={index}
-            className="relative w-full h-80 bg-gray-300 overflow-hidden rounded-lg hover:scale-[102%] duration-200"
-          >
-            <img
-              src={person.image}
-              alt={person.name}
-              className="w-full h-full object-cover"
-            />
-            <div className="absolute inset-0 bg-black bg-opacity-30"></div>
-            <div className="absolute bottom-4 left-4 text-white">
-              <h2 className="text-lg font-semibold">{person.name}</h2>
-              <p className="text-sm">{person.position}</p>
-            </div>
+      <div
+        key={selectedTab}
+        className="animate-fade-in transition-all duration-500 ease-in-out"
+      >
+        {selectedTab === "Partners" && (
+          <div className="flex flex-wrap justify-center gap-12">
+            {filteredEmployees.map((person, index) => (
+              <div
+                key={index}
+                className="relative w-72 h-96 bg-gray-300 overflow-hidden rounded-lg hover:scale-[102%] duration-200"
+              >
+                <img
+                  src={person.image}
+                  alt={person.name}
+                  className="w-full h-full object-cover"
+                />
+                <div className="absolute inset-0 bg-black bg-opacity-10"></div>
+                <div className="absolute bottom-4 left-4 text-white">
+                  <h2 className="text-lg font-semibold">{person.name}</h2>
+                  <p className="text-sm">{person.position}</p>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        )}
+
+        {selectedTab === "Associates" && (
+          <div className="space-y-8 px-5 sm:px-14 md:px-20 lg:px-44">
+            {filteredEmployees.map((person, index) => (
+              <div
+                key={index}
+                className="flex flex-col md:flex-row items-start gap-6"
+              >
+                <div className="w-full md:w-1/3 h-96 bg-gray-300 overflow-hidden rounded-lg">
+                  <img
+                    src={person.image}
+                    alt={person.name}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="flex-1">
+                  <h2 className="text-lg font-semibold">{person.name}</h2>
+                  <p className="text-sm text-gray-600 mb-2">
+                    {person.position}
+                  </p>
+                  <p className="text-gray-700">{person.bio}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </main>
   );
